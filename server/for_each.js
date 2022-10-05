7a7d7955a5ec7822d68941b5e7f2d815e7efb763
erikmya <erik.maya@librerialeo.com.mx>
2022-10-05 17:06:10 -0500

    clase6 ejercicio tax

--- server/for_each.js
+++ server/for_each.js
@@ -17,3 +17,22 @@ const array = ['1', '2', '3','4'];
 const numerosArray = numeros(array);
 
 console.log(numerosArray);
+
+//ejercicio clase 5
+
+const tienda = [ {
+    Name:'pantalon',
+    price: 1000,
+    stock: 5000,
+    }
+];
+
+const newArray = tienda.map(item1 => {
+    return{
+        ...item1,
+        tax: Math.trunc( item1.price * 0.19),
+    };
+});
+
+console.log('Articulo: ', tienda);
+console.log('Taxes: ', newArray);
