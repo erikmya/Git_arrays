2cf88b57c7e51537a75722c1cada7be7d36c2032
erikmya <erik.maya@librerialeo.com.mx>
2022-10-06 11:30:33 -0500

    Clase 11 ejercicio sumas, reduce

--- server/reduce.js
+++ server/reduce.js
@@ -10,3 +10,25 @@ console.log(sum);
 
 const rta = totals.reduce((sum, element) => sum + element, 0)
 console.log('rta', rta);
+
+// Clase 11
+
+const numeros = [1,1,1];
+const par = [2,4,6];
+
+function func(numeros) {
+    return numeros.reduce((sum, element) => sum + element, 0);
+}
+const pares = func(par);
+const suma = func(numeros);
+console.log(suma)
+console.log(pares)
+
+// let sum = 0;
+
+// const tres = numeros.reduce((sum, element) => sum + element, 0);
+// console.log('tres', tres);
+
+// const pares = par.reduce((sum, element) => sum + element, 0);
+//console.log('pares', pares); 
+
