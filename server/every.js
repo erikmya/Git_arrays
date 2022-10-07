c0bf2c1d014741682fccb58273bb161a2d2482d4
erikmya <erik.maya@librerialeo.com.mx>
2022-10-07 15:02:35 -0500

    Reto clase 16

--- server/every.js
+++ server/every.js
@@ -34,3 +34,21 @@ const team = [
  
 const ninos = team.every(item => item.age <= 15)
 console.log('reto', ninos);
+
+//reto clase 16
+
+const num1 = [2, 4, 6, 8, 10];
+const num2 = [1, 3, 5, 7, 10, 11];
+const num3 = [1, 3, 5];
+
+function par(numbers){
+    return numbers.every(item => item % 2 === 0);
+} 
+
+const par1 = par(num1);
+const par2 = par(num2);
+const par3 = par(num3);
+
+console.log(par1);
+console.log(par2);
+console.log(par3);
