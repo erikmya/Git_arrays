d4e9857c6aa7f20c558fcf4e2b42923b90b68073
erikmya <erik.maya@librerialeo.com.mx>
2022-10-07 14:06:10 -0500

    Clase 15 y Reto

---
+++ server/every.js
@@ -0,0 +1,36 @@
+const numbers = [1,30,39,29,10,13];
+
+let rta = true;
+for (let index = 0; index < numbers.length; index++) {
+    const element = numbers[index];
+    if (element >= 40) {
+        rta = false
+    }
+}
+console.log('for', rta);
+const rta2 = numbers.every(item => item <= 40)
+console.log('rta2', rta2);
+
+// reto clase 15
+
+const team = [
+    {
+      name: "Nicolas",
+      age: 12,
+    },
+    {
+      name: "Andrea",
+      age: 8,
+    },
+    {
+      name: "Zulema",
+      age: 2,
+    },
+    {
+      name: "Santiago",
+      age: 18,
+    },
+  ];
+ 
+const ninos = team.every(item => item <= 15)
+console.log('reto', ninos);
