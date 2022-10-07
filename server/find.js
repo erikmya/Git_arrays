3473f77363bd13f6a9ef5377bf3b73d26c62e5a0
erikmya <erik.maya@librerialeo.com.mx>
2022-10-07 15:24:22 -0500

    Clase 17 Find

---
+++ server/find.js
@@ -0,0 +1,43 @@
+const numbers = [1,30,49,29,10,13];
+
+
+for (let index = 0; index < numbers.length; index++) {
+    const element = numbers[index];
+    if (element === 13) {
+        rta = element;
+        break;
+    }
+    
+}
+console.log('for', rta);
+const rta2 = numbers.find(item => item === 13)
+console.log('find', rta2);
+
+const products = [
+    {
+      name: "Pizza",
+      price: 12,
+      id: '🍕'
+    },
+    {
+      name: "Burger",
+      price: 23,
+      id: '🍔'
+    },
+    {
+      name: "Hot dog",
+      price: 34,
+      id: '🌭'
+    },
+    {
+      name: "Hot cakes",
+      price: 355,
+      id: '🥞'
+    },
+  ];
+
+const rta3 = products.find(item => item.id === '🥞')
+console.log('find', rta3);
+const rta4 = products.findIndex(item => item.id === '🥞')
+console.log('find', rta4);
+  
