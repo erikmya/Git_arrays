b65aac76986adf4826d38b7fc3eee609d8fb1269
erikmya <erik.maya@librerialeo.com.mx>
2022-10-07 17:03:29 -0500

    Reto clase 21

--- server/join.js
+++ server/join.js
@@ -13,5 +13,25 @@ console.log('join', rta);
 
 const title = 'Curso de manipulacion de arrays';
 
-const urlFinal = title.split(' ').join('-').toLocaleLowerCase();
+const urlFinal = title.split(' ').join('-').toLowerCase();
 console.log(urlFinal);
+
+// Reto Clase 21
+
+const title1 = 'La forma de correr Python';
+const title2 = 'La API para nunca parar de aprender';
+const title3 = 'Curso de arrays';
+
+function url(titulo) {
+    return titulo.split(' ').join('-').toLowerCase();
+    
+}
+
+const dir = url(title1);
+const dir2 = url(title2);
+const dir3 = url(title3);
+
+console.log(dir);
+console.log(dir2);
+console.log(dir3);
+
