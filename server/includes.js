27798850aa2df0752ac6bc8e259ae7a54b3633d9
erikmya <erik.maya@librerialeo.com.mx>
2022-10-07 15:59:36 -0500

    Clase 19 reto

--- server/includes.js
+++ server/includes.js
@@ -12,3 +12,18 @@ console.log('for', includeInArray);
 
 const rta = pets.includes('dog')
 console.log('includes', rta);
+
+//Reto clase 19
+
+const nombres = ['Ana lava la tina', 'ana'];
+const nombres2 = ['Nicolas', 'ana'];
+const nombres3 = ['Santiago', 'tiago']
+function anita(palabras) {
+    return palabras.includes('ana');
+}
+const name1 = anita(nombres);
+const name2 = anita(nombres2);
+const name3 = anita(nombres3);
+console.log(name1);
+console.log(name2);
+console.log(name3);
